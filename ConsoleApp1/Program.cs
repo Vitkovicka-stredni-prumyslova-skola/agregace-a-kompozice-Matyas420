c3ff0c487ed0d40a2e3c64a2f6110aea4aae9c82
Matyas420 <98987@student.vitkovickastredni.cz>
2026-02-24 21:09:23 +0100

    TEST 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,9 +5,14 @@
         
         public static void Main(string[] args)
         {
-        
+            var student1 = new Student("Petr", "Novotný", 3);
+            var studnet2 = new Student("Lucie", "Marná", 3);
+            var trida = new Trida("3.A");
 
-        
+            // TEST 1
+            trida.TridniKniha.ZapisDochazku(student1, new DateOnly(2026, 1, 17), true);
+            trida.TridniKniha.ZapisDochazku(studnet2, new DateOnly(2026, 2, 24), false);
+
         }
     }
 }
--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66c64b595455632cadea4257dd76c3e7b37d7bba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f08275bc01b6cab0090e62fccfcad7a429e6aee3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
