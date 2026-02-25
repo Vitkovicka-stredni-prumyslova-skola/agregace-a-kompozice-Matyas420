d579a9849a1d7b8ccb066e134d161897416275a5
Matyas420 <98987@student.vitkovickastredni.cz>
2026-02-25 15:55:41 +0100

    update.nevim.čeho

--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f08275bc01b6cab0090e62fccfcad7a429e6aee3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3ff0c487ed0d40a2e3c64a2f6110aea4aae9c82")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
